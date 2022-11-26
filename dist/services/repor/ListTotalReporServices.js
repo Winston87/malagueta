1173abcdc8f9800fd966b1096a45405bd85e0f91
winston salem <winstonwill94@gmail.com>
2022-11-26 17:06:03 -0300

    atualizaçao

--- dist/services/repor/ListTotalReporServices.js
+++ dist/services/repor/ListTotalReporServices.js
@@ -21,6 +21,7 @@ class ListTotalReporServices {
             const repor = prisma_1.default.$queryRaw(client_1.Prisma.sql `select produtos.id, produtos.name, produtos.price, sum(relatorio.amount) as amount, sum(relatorio.sales) total_sale, relatorio.created_at
             from produtos inner join itens on produtos.id = itens.product_id
             inner join relatorio on itens.id = relatorio.item_id
+            inner join pedidos on itens.ordem_id = pedidos.id
             where pedidos.status = true
             and pedidos.draft = true 
             group by produtos.id, produtos.name, produtos.price, relatorio.created_at`);
