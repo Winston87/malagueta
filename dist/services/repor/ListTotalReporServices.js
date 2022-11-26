c92fcf7947038c72c66a8a545a4bdc94dec7621f
winston salem <winstonwill94@gmail.com>
2022-11-26 16:55:52 -0300

    atualizaçao

--- dist/services/repor/ListTotalReporServices.js
+++ dist/services/repor/ListTotalReporServices.js
@@ -20,7 +20,9 @@ class ListTotalReporServices {
         return __awaiter(this, void 0, void 0, function* () {
             const repor = prisma_1.default.$queryRaw(client_1.Prisma.sql `select produtos.id, produtos.name, produtos.price, sum(relatorio.amount) as amount, sum(relatorio.sales) total_sale, relatorio.created_at
             from produtos inner join itens on produtos.id = itens.product_id
-            inner join relatorio on itens.id = relatorio.item_id 
+            inner join relatorio on itens.id = relatorio.item_id
+            where pedidos.status = true
+            and pedidos.draft = true 
             group by produtos.id, produtos.name, produtos.price, relatorio.created_at`);
             return repor;
         });
--- src/services/repor/ListTotalReporServices.ts
+++ src/services/repor/ListTotalReporServices.ts
@@ -9,7 +9,9 @@ class ListTotalReporServices {
         const repor = prismaClient.$queryRaw(Prisma.sql
             `select produtos.id, produtos.name, produtos.price, sum(relatorio.amount) as amount, sum(relatorio.sales) total_sale, relatorio.created_at
             from produtos inner join itens on produtos.id = itens.product_id
-            inner join relatorio on itens.id = relatorio.item_id 
+            inner join relatorio on itens.id = relatorio.item_id
+            where pedidos.status = true
+            and pedidos.draft = true 
             group by produtos.id, produtos.name, produtos.price, relatorio.created_at`);
 
              
